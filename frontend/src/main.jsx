40b366823f8940796d0e61e548f4cc4883277a34
adam323233 <rekotyea32@gmail.com>
2025-01-17 13:08:14 +0100

    asdasdasdsadas

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import { BrowserRouter, Routes, Route } from "react";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="" element={<App />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
